14f28ad1adb851b4a65656fbf70000f0963ee358
Sükein Alex <suxy@wanari.com>
2017-09-28 18:22:42 +0200

    project generated with Spring Initializr

---
+++ src/main/java/com/wanari/specification/example/SpecificationExampleApplication.java
@@ -0,0 +1,12 @@
+package com.wanari.specification.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecificationExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wanari/specification/example/SpecificationExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wanari.specification.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpecificationExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
